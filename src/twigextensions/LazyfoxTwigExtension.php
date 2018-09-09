d26a640191f00b66c3142ddb5082b5b029d78db7
janhuenermann <jan@huenermann.de>
2018-09-09 17:34:50 +0200

    update

--- src/twigextensions/LazyfoxTwigExtension.php
+++ src/twigextensions/LazyfoxTwigExtension.php
@@ -80,7 +80,7 @@ class LazyfoxTwigExtension extends \Twig_Extension
     }
 
     public function getBase64(Asset $asset) {
-        $file = $asset->url;
+        $file = $asset->getUrl();
         $binary = file_get_contents($file);
         // Return the string.
         return sprintf('data:image/%s;base64,%s', $asset->getExtension(), base64_encode($binary));
