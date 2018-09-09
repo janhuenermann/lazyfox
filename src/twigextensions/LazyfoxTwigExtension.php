8bd9a0ab9039dfa1ea6f184031c9a8eb8fb19862
janhuenermann <jan@huenermann.de>
2018-09-10 00:55:29 +0200

    update

--- src/twigextensions/LazyfoxTwigExtension.php
+++ src/twigextensions/LazyfoxTwigExtension.php
@@ -76,8 +76,8 @@ class LazyfoxTwigExtension extends \Twig_Extension
 
         echo 
            '<picture class="lazyfox --not-loaded">
-                <div class=lazyfox-placeholder style="padding-bottom: ' . ($h / $w * 100) . '%"></div>
-                <img data-srcset="' . $srcset . '" data-src="' .  $asset->getUrl($transform) . '" src="' . $this->getBase64($asset, $transform) . '">
+                <div class=lazyfox-placeholder style="padding-bottom: ' . ($h / $w * 100) . '%; background-image: url(' . $this->getBase64($asset, $transform) . ')"></div>
+                <img data-srcset="' . $srcset . '" data-src="' .  $asset->getUrl($transform) . '">
             </picture>';
 
         Craft::$app->view->registerAssetBundle(LazyfoxAsset::class);
