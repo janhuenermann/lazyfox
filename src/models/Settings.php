2e1d433f0261292cc886dc4a674e6857e2ef8619
janhuenermann <jan@huenermann.de>
2018-09-10 17:03:18 +0200

    update

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -39,7 +39,7 @@ class Settings extends Model
      *
      * @var string
      */
-    public $sourceSet = [ ['handle' => '1x', 'size' => '50%'], ['handle' => '1.5x', 'size' => '75%'], ['handle' => '2x', 'size' => '100%'] ];
+    public $sourceSet = [ ['handle' => '0.5x', 'size' => '25%', 'minWidth' => 400], ['handle' => '1x', 'size' => '50%', 'minWidth' => 200], ['handle' => '2x', 'size' => '100%', 'minWidth' => 0] ];
 
     public $previewType = 'pixelated';
 
