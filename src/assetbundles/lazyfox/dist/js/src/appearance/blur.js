d2f29012f3cf1f14485890d50d40cded0970f963
janhuenermann <jan@huenermann.de>
2018-09-10 22:55:10 +0200

    update

--- src/assetbundles/lazyfox/dist/js/src/appearance/blur.js
+++ src/assetbundles/lazyfox/dist/js/src/appearance/blur.js
@@ -1,10 +1,18 @@
 import * as stackblur from '../vendor/stackblur'
 
 const radius = 20
+const scale = 10
 
 export default function activate(lf) {
 	let canvas = document.createElement('canvas')
 	lf.container.insertBefore(canvas, lf.placeholder.nextSibling)
 
-	stackblur.image(lf.placeholder, canvas, radius, false)
+	let w = lf.placeholder.naturalWidth;
+	let h = lf.placeholder.naturalHeight;
+
+	canvas.width = w * scale;
+	canvas.height = h * scale;
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+	
+	StackBlur.canvasRGB(canvas, 0, 0, canvas.width, canvas.height, radius);
 }
