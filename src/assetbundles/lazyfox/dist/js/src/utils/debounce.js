ce8003e81bf226f3253b1f37fbd3c83f7a5becea
janhuenermann <jan@huenermann.de>
2018-09-10 13:33:41 +0200

    update

--- src/assetbundles/lazyfox/dist/js/src/utils/debounce.js
+++ src/assetbundles/lazyfox/dist/js/src/utils/debounce.js
@@ -1,25 +1,64 @@
+// http://modernjavascript.blogspot.de/2013/08/building-better-debounce.html
+// 
 export function debounce(func) {
-	var timeout, timestamp;
-	var wait = 99;
-	var run = function(){
-		timeout = null;
-		func();
-	};
+    // we need to save these in the closure
+    var timeout, context, timestamp;
+    var wait = 100;
 
-	var later = function() {
-		var last = Date.now() - timestamp;
-		if (last < wait) {
-			setTimeout(later, wait - last);
-		} else {
-			(window.requestIdleCallback || run)(run);
-		}
-	};
+    var exec = function() {
+        timeout = null;
+        func.call(context);
+    };
 
-	return function() {
-		timestamp = Date.now();
+    // this is where the magic happens
+    var later = function() {
+        // how long ago was the last call
+        var last = (new Date()) - timestamp;
 
-		if (!timeout) {
-			timeout = setTimeout(later, wait);
-		}
-	};
+        // if the latest call was less that the wait period ago
+        // then we reset the timeout to wait for the difference
+        if (last < wait) {
+            timeout = setTimeout(later, wait - last);
+            // or if not we can null out the timer and run the latest
+        } else {
+        	if (window.requestIdleCallback) window.requestIdleCallback(exec);
+            else exec();
+        }
+    };
+
+    return function () {
+        // save details of latest call
+        context = this;
+        timestamp = new Date();
+
+        // we only need to set the timer now if one isn't already running
+        if (!timeout) {
+            timeout = setTimeout(later, wait);
+        }
+    }
+}
+export function debounce(func) {
+    var timeout, timestamp;
+    var wait = 99;
+    var run = function() {
+        timeout = null;
+        func();
+    };
+
+    var later = function() {
+        var last = Date.now() - timestamp;
+        if (last < wait) {
+            setTimeout(later, wait - last);
+        } else {
+            (window.requestIdleCallback || run)(run);
+        }
+    };
+
+    return function() {
+        timestamp = Date.now();
+
+        if (!timeout) {
+            timeout = setTimeout(later, wait);
+        }
+    };
 };
