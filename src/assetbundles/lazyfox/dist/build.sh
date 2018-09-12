00772d39854d8392d66f287dc7f49993ab2631f5
janhuenermann <jan@huenermann.de>
2018-09-12 18:13:52 +0200

    update

--- src/assetbundles/lazyfox/dist/build.sh
+++ src/assetbundles/lazyfox/dist/build.sh
@@ -1,7 +1,9 @@
 mkdir tmp
 
+cd js
 rollup -c
-uglifyjs lazyfox.js -o lazyfox.min.js
+uglifyjs js/lazyfox.js -o js/lazyfox.min.js
+cd ..
 
 npx postcss css/lazyfox.css --config css/ -d tmp/
 cleancss -o css/lazyfox.min.css tmp/lazyfox.css
