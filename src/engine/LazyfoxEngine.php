963fa64a54e1561f7ae2616e6405d719cf1cfd3d
janhuenermann <jan@huenermann.de>
2018-09-12 18:38:44 +0200

    update

--- src/engine/LazyfoxEngine.php
+++ src/engine/LazyfoxEngine.php
@@ -36,7 +36,7 @@ class LazyfoxEngine {
         foreach ($srcset as $size) {
         	$a = $imager->transformImage($asset, [ 'width' => $size, 'format' => 'jpg', 'interlace' => 'partition' ]);
             // $t = static::getScaledDownTransform($transform, ceil($size));
-            $url = Craft::alias('@web' . $a->getUrl());
+            $url = Craft::getAlias('@web' . $a->getUrl());
             $attr[] = $url . ' ' . $size . 'w';
         }
 
