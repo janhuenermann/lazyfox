97f3b26a613df88570dcba41095e05fda82c032c
janhuenermann <jan@huenermann.de>
2018-09-12 18:39:04 +0200

    update

--- src/twigextensions/LazyfoxTwigExtension.php
+++ src/twigextensions/LazyfoxTwigExtension.php
@@ -89,7 +89,7 @@ class LazyfoxTwigExtension extends \Twig_Extension
         $sizes = LazyfoxEngine::getSourceSet($settings, max($w, $h));
         $srcset = LazyfoxEngine::produceSourceSet($sizes, $asset, $transform);
 
-        $classes = " --no-progress --transition --sized --$previewType";
+        $classes = " --transition --sized --$previewType";
 
         echo 
            '<picture class="lazyfox --not-loaded' . $classes . '" data-type="' . $previewType . '">
